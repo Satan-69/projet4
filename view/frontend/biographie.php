11df29ace34e8c55dbf5d6156711fcd3753276d5
satan-69 <hopfner.charles@gmail.com>
2020-03-30 13:10:21 +0200

    mentions légales & recaptcha on contact form

--- view/frontend/biographie.php
+++ view/frontend/biographie.php
@@ -1,7 +1,7 @@
 <?php $title = 'Ma vie';
 ob_start();
 ?>
-<h1 class="display-4">Voici ma vie.</h1>
+<h1 class="m-4 display-4 animated fadeIn slow">Voici ma vie.</h1>
 
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nihil ducimus mollitia velit itaque distinctio dolore
     molestiae ratione veritatis placeat eum sed,
