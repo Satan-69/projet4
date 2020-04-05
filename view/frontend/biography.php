fba3118c7f6545b8b8dec44c3c4d9070ee50e4d7
satan-69 <hopfner.charles@gmail.com>
2020-04-05 16:44:21 +0200

    corrections biographie

--- view/frontend/biography.php
+++ view/frontend/biography.php
@@ -1,8 +1,8 @@
 <?php $title = 'Ma vie';
 ob_start();
 ?>
-<h1 class="m-4 display-4 animated fadeIn slow">Voici ma vie.</h1>
-<hr class="hr-text m-5" data-content="Biographie">
+<h1 class="m-4 display-4 animated fadeIn slow">Biographie</h1>
+<hr class="hr-text m-5" data-content="Histoire">
 <p>Louis De La Forte Roche, plus connu sous son nom d'écrivain Jean Forteroche (qu'il choisit en clin d'oeil à son idole
     <a href="https://fr.wikipedia.org/wiki/Jean_Rochefort"><u>Jean Rochefort</u></a>) est un écrivain français né à
     Toulon en le 17 Juillet 1971.
@@ -16,17 +16,17 @@ ob_start();
     Il tombe amoureux de New York, et décide de s'installer dans le quartier de Brooklyn où il rencontre la journaliste
     américaine Suzanne Bauer. 5 ans et quelques voyages plus tard, ils s'installent tous les deux à Paris, où il réside
     encore actuellement. En 1998, Suzanne donne naissance à Julie Forteroche.
-    Après avoir enchainé différents jobs alimentaires, il décide de se replonger dans sa passion : l'écriture. Il
+    En 2010, il décide de revenir à sa première passion : l'écriture. Il
     enchaîne quelques pièces de théâtre en tant que scénariste, écrit des bandes dessinnées, un livre de nouvelles
     autobiographiques, puis quelques romans. Il coproduit aussi deux documentaires avec son épouse. </p><br>
 <p> En 2020, il décide de s'essayer à un autre style d'écriture, et crée ce site sur lequel il publie son nouveau roman
     chapitre par chapitre : <a href="chapitres.php"><u>Billet simple pour l'Alaska</u></a>.
-    Il a reçu le grand prix des lecteurs de "Elle"
-    pour son premier roman <a href="accueil.php?#books"><u>Alice sur le fil</u></a> en 2015 et le prix de Maison de la
+</p><p>Il a reçu le grand prix des lecteurs de "Elle"
+    pour son deuxième roman <a href="accueil.php?#books"><u>Alice sur le fil</u></a> en 2015 et le prix Maison de la
     presse pour <a href="accueil.php?#books"><u>Le Palais de la lune</u></a> en 2018.
 </p>
 <hr class="hr-text m-5" data-content="Bibliographie">
-<ul id="booktypes" class="llist-unstyled">
+<ul id="booktypes" class="list-unstyled">
     <li class="booktypes">
         <h5>Pièces de théâtre</h5>
     </li>
@@ -52,9 +52,9 @@ ob_start();
     </li>
     <ul>
         <li>2013 : Les Routes d'Amato</li>
-        <li>2015 : Alice sur le Fil</li>
+        <li>2015 : Alice sur le Fil, récompensé par le prix des lecteurs de "Elle"</li>
         <li>2017 : Les Rêves Rouges</li>
-        <li>2018 : Le Palais de la Lune</li>
+        <li>2018 : Le Palais de la Lune, récompensé par le prix Maison de la Presse</li>
         <li>2020 : Billet simple pour l'Alaska</li>
     </ul>
     <li class="booktypes">
